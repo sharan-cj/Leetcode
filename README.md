57c09a7676878964e536a37aaed1b4d59c5d2909
sharan-cj <sharan.cj.gowda@gmail.com>
2024-07-05 23:51:19 +0530

    153. Find Minimum in Rotated Sorted Array

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
+### Binary Search
+
+- 153. Find Minimum in Rotated Sorted Array
+- 875. Koko Eating Bananas
 - 74. Search a 2D Matrix
 - 13. Roman to Integer
+
+### Random - EASY
+
 - 704. Binary Search
+
+### Stack
+
 - 853.  Car Fleet
 - 739.  Daily Temperatures
 - 22. Generate Parentheses
--- binarySearch.js
+++ binarySearch.js
@@ -1,42 +1,93 @@
-const searchMatrix = function (matrix, target) {
-  const m = matrix[0].length;
-  const n = matrix.length;
-  const len = m * n;
+// // L153. Find Minimum in Rotated Sorted Array
+// const findMin = function (nums) {
+//   let l = 0;
+//   let r = nums.length - 1;
+//   let res = nums[l];
 
-  const getValueFromIndex = (i) => {
-    let row = Math.floor(i / m);
-    const column = i % m;
-    return matrix[row][column];
-  };
+//   while (l < r) {
+//     const mid = Math.floor(l + (r - l) / 2);
+//     if (nums[mid] < nums[r]) {
+//       r = mid;
+//     } else {
+//       l = mid + 1;
+//     }
+//   }
+//   res = nums[l];
+//   return res;
+// };
 
-  let left = 0;
-  let right = len - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (getValueFromIndex(mid) === target) {
-      return true;
-    }
+// console.log(findMin([4, 5, 6, 7, 0, 1, 2]));
 
-    if (target > getValueFromIndex(mid)) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
-  }
+// // L875. Koko Eating Bananas
+// const minEatingSpeed = function (piles, h) {
+//   const findHoursToFinish = (k) => {
+//     let h = 0;
+//     for (let i = 0; i < piles.length; i++) {
+//       h += Math.ceil(piles[i] / k);
+//     }
+//     return h;
+//   };
 
-  return false;
-};
+//   let l = 1;
+//   let r = Math.max(...piles);
 
-console.log(
-  searchMatrix(
-    [
-      [1, 3, 5, 7],
-      [10, 11, 16, 20],
-      [23, 30, 34, 60],
-    ],
-    3
-  )
-);
+//   let res = r;
+//   while (l <= r) {
+//     const mid = Math.floor((l + r) / 2);
+//     const htf = findHoursToFinish(mid);
+//     console.log(l, r, mid, htf);
+//     if (htf > h) {
+//       l = mid + 1;
+//     } else {
+//       res = Math.min(res, mid);
+//       r = mid - 1;
+//     }
+//   }
+//   return res;
+// };
+
+// console.log(minEatingSpeed([312884470], 312884469));
+
+// L74. Search a 2D Matrix
+// const searchMatrix = function (matrix, target) {
+//   const m = matrix[0].length;
+//   const n = matrix.length;
+//   const len = m * n;
+
+//   const getValueFromIndex = (i) => {
+//     let row = Math.floor(i / m);
+//     const column = i % m;
+//     return matrix[row][column];
+//   };
+
+//   let left = 0;
+//   let right = len - 1;
+//   while (left <= right) {
+//     const mid = Math.floor((left + right) / 2);
+//     if (getValueFromIndex(mid) === target) {
+//       return true;
+//     }
+
+//     if (target > getValueFromIndex(mid)) {
+//       left = mid + 1;
+//     } else {
+//       right = mid - 1;
+//     }
+//   }
+
+//   return false;
+// };
+
+// console.log(
+//   searchMatrix(
+//     [
+//       [1, 3, 5, 7],
+//       [10, 11, 16, 20],
+//       [23, 30, 34, 60],
+//     ],
+//     3
+//   )
+// );
 
 // // L704. Binary Search
 // const search = function (nums, target) {
